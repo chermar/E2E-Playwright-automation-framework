ebbf0d76e0b1e59f78777808a9321610753eb0a3
M Cherma Rajan <M.Cherma.Rajan@gds.ey.com>
2026-01-02 07:24:40 +0530

    added headless = true

--- playwright.config.ts
+++ playwright.config.ts
@@ -44,7 +44,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-    headless:false,
+    headless:true,
     browserName:'chromium',
     screenshot:'only-on-failure'
   },
